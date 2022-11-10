5c4c379abc0801a998ff63ae9c73c885cc631213
Aishwarya <117883578+AishwaryaU123@users.noreply.github.com>
2022-11-10 09:16:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents')
+streamlit.title('My parents Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli Sambar')
+streamlit.text('Masala Dosa')
+streamlit.text('Uttappam')
